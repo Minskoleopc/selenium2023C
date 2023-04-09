f82f8c48d0091f90c8f47b954ea78e490f721ef0
Minskoleopc <minskoleopc@gmail.com>
2023-04-09 13:00:45 +0530

    added

---
+++ src/alerts.java
@@ -0,0 +1,127 @@
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class alerts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ankurjain\\Desktop\\chromedriver\\chromedriver.exe");
+//		ChromeDriver driver = new ChromeDriver();
+//		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+//		driver.findElement(By.cssSelector("button[onclick=\"jsAlert()\"]")).click();
+//		Alert alert = driver.switchTo().alert();
+//		driver.switchTo().alert().accept();
+//		String text = driver.findElement(By.id("result")).getText();
+//		if(text.equals("You successfully clicked an alert")) {
+//			System.out.println("Testcase pass");
+//		}
+//		else {
+//			System.out.println("Testcase fail");
+//		}
+//		
+		
+		
+//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ankurjain\\Desktop\\chromedriver\\chromedriver.exe");
+//		ChromeDriver driver = new ChromeDriver();
+//		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+//		driver.findElement(By.cssSelector("button[onclick=\"jsConfirm()\"]")).click();
+//		Alert alert = driver.switchTo().alert();
+//		driver.switchTo().alert().accept();
+//		String text = driver.findElement(By.id("result")).getText();
+//		if(text.equals("You clicked: Ok")) {
+//			System.out.println("Testcase pass");
+//		}
+//		else {
+//			System.out.println("Testcase fail");
+//		}
+
+		
+//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ankurjain\\Desktop\\chromedriver\\chromedriver.exe");
+//		ChromeDriver driver = new ChromeDriver();
+//		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+//		driver.findElement(By.cssSelector("button[onclick=\"jsConfirm()\"]")).click();
+//		Alert alert = driver.switchTo().alert();
+//		driver.switchTo().alert().accept();
+//		String text = driver.findElement(By.id("result")).getText();
+//		if(text.equals("You clicked: Ok")) {
+//			System.out.println("Testcase pass");
+//		}
+//		else {
+//			System.out.println("Testcase fail");
+//		}
+		
+		
+//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ankurjain\\Desktop\\chromedriver\\chromedriver.exe");
+//		ChromeDriver driver = new ChromeDriver();
+//		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+//		driver.findElement(By.cssSelector("button[onclick=\"jsConfirm()\"]")).click();
+//		Alert alert = driver.switchTo().alert();
+//		driver.switchTo().alert().dismiss();
+//		String text = driver.findElement(By.id("result")).getText();
+//		if(text.equals("You clicked: Cancel")) {
+//			System.out.println("Testcase pass");
+//		}
+//		else {
+//			System.out.println("Testcase fail");
+//		}
+//		
+//		
+//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ankurjain\\Desktop\\chromedriver\\chromedriver.exe");
+//		ChromeDriver driver = new ChromeDriver();
+//		String txt = 	"hello";
+//		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+//		driver.findElement(By.cssSelector("button[onclick=\"jsPrompt()\"]")).click();
+//		driver.switchTo().alert().sendKeys(txt);
+//		driver.switchTo().alert().accept();
+//		String text = driver.findElement(By.id("result")).getText();
+//		if(text.contains(txt)) {
+//			System.out.println("Test case pass");
+//		}
+//		
+		
+		
+//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ankurjain\\Desktop\\chromedriver\\chromedriver.exe");
+//		ChromeDriver driver = new ChromeDriver();
+//		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+//		driver.findElement(By.cssSelector("button[onclick=\"jsPrompt()\"]")).click();
+//		driver.switchTo().alert().dismiss();
+//		String text = driver.findElement(By.id("result")).getText();
+//		if(text.contains("null")) {
+//			System.out.println("Test case pass");
+//		}
+//		
+//		
+		
+		
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+	
+
+}
